5ccfb3abed65ad368149b784c8d478242afab7d1
aulbert <aalbertuntung@gmail.com>
2016-12-20 01:43:20 -0500

    simplified all columns name
    reordered folders, database purely orm style now

--- lib/dbsetup_orm.py
+++ lib/dbsetup_orm.py
@@ -20,15 +20,21 @@ https://github.com/coleifer/peewee
 Redo SQLs
 
 This database is pretty much static, read-only
+PrimaryKeyField is autoincrementing
 """
 from peewee import *
+from dbsetup_helper import *
 import csv
 import os
 import os.path
 import sys
 
-database_filename = 'master_peewee.db'
+# Create new sqlite everytime
+database_filename = 'master_orm.sqlite'
+if os.path.isfile(database_filename): 
+	os.system('rm master_orm.sqlite')
 database = SqliteDatabase(database_filename)
+database.set_autocommit(False)
 
 """
 Create all the necessary tables for Pokemon. Declare all tables here.
@@ -46,35 +52,8 @@ def create_tables():
 							PokemonCompetitive,
 							PokemonSprite,
 							PokemonType,
-							Type])
-
-
-"""
-Automatically fill all the tables using the files in the csv folder.
-"""
-def build():
-	csvs = glob.glob(os.path.join(DATA_LOCATION, '*'))
-	for csv in csvs:
-		print "Building " + csv
-		# Determine database_name # oman
-		database_name = csv.split("/")[-1].split(".")[0]
-		build_aux(csv, database_name)
-
-
-"""
-Helper functions for build()
-"""
-def with_iter(context, iterable=None):
-    if iterable is None:
-        iterable = context
-    with context:
-        for value in iterable:
-            yield value
-
-
-def load_data(filename):
-    # with_iter closes the file when it has finished
-    return csv.reader(with_iter(open(filename, 'rt')), delimiter=',')
+							Type],
+							safe=True)
 
 
 class PokeModel(Model):
@@ -84,7 +63,7 @@ class PokeModel(Model):
 
 # Definition
 class Generation(PokeModel):
-	pk = PrimaryKeyField()
+	pk = IntegerField(primary_key=True) 
 	name = CharField()
 
 
@@ -92,15 +71,15 @@ class Generation(PokeModel):
 class Type(PokeModel):
 	pk = IntegerField(primary_key=True)
 	name = CharField()
-	generation = ForeignKeyField(Generation, to_field='pk')
+	gen = ForeignKeyField(Generation, to_field='pk')
 
 
 # Definition
 class Ability(PokeModel):
-	pk = PrimaryKeyField()
+	pk = IntegerField(primary_key=True)
 	name = CharField()
 	flavour_text = CharField()
-	generation = ForeignKeyField(Generation, to_field='pk', related_name='ability_gen_introduced')
+	gen = ForeignKeyField(Generation, to_field='pk', related_name='ability_gen_introduced')
 
 
 # Definition
@@ -111,14 +90,14 @@ class Pokemon(PokeModel):
 	hp = IntegerField()
 	attack = IntegerField()
 	defense = IntegerField()
-	special_attack = IntegerField()
-	special_defense = IntegerField()
+	sp_attack = IntegerField()
+	sp_defense = IntegerField()
 	speed = IntegerField()
 	bst = IntegerField()
 	height = IntegerField() 
 	weight = IntegerField() 
-	base_experience = IntegerField()
-	generation = ForeignKeyField(Generation, to_field='pk')
+	base_exp = IntegerField()
+	gen = ForeignKeyField(Generation, to_field='pk')
 	is_default = BooleanField()
 
 # Definition
@@ -131,15 +110,15 @@ class Classification(PokeModel):
 class Competitive(PokeModel):
 	pk = IntegerField(primary_key=True)
 	name = CharField()
-	generation = ForeignKeyField(Generation, to_field='pk')
+	gen = ForeignKeyField(Generation, to_field='pk')
 
 
 # Many to many, sorta
 class FinalEvolution(PokeModel):
 	pk = IntegerField(primary_key=True)
 	pokemon = ForeignKeyField(Pokemon)
-	generation_start = ForeignKeyField(Generation, to_field='pk', related_name='evo_gen_start')
-	generation_until = ForeignKeyField(Generation, to_field='pk', related_name='evo_gen_until') 
+	gen_start = ForeignKeyField(Generation, to_field='pk', related_name='gen_start')
+	gen_until = ForeignKeyField(Generation, to_field='pk', related_name='gen_until') 
 
 
 # Many to Many
@@ -169,10 +148,18 @@ class PokemonType(PokeModel):
 	pokemon = ForeignKeyField(Pokemon)
 	pokemon_type = ForeignKeyField(Type)
 	slot = IntegerField()
-	generation_start = ForeignKeyField(Generation, to_field='pk', related_name='type_gen_start')
-	generation_until = ForeignKeyField(Generation, to_field='pk', related_name='type_gen_until')
+	gen_start = ForeignKeyField(Generation, to_field='pk', related_name='gen_start')
+	gen_until = ForeignKeyField(Generation, to_field='pk', related_name='gen_until')
+
+
+# Many to Many
+class PokemonAbility(PokeModel):
+	pk = IntegerField(primary_key=True)
+	pokemon = ForeignKeyField(Pokemon)
+	ability = ForeignKeyField(Ability)
+	slot = IntegerField()
 
 
 if __name__ == '__main__':
 	# Fill database with csv
-	pass
+	create_tables()
